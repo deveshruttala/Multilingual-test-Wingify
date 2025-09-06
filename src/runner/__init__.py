c031be1d89c1230aeebf6ba9938eac06dfcf657a
Devesh Ruttala <deveshruttala@gmail.com>
2025-09-07 02:47:21 +0530

    makes

--- src/runner/__init__.py
+++
@@ -1,1 +0,0 @@
-# This file makes the runner directory a Python package
--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--update-snapshots",
-        action="store_true",
-        default=False,
-        help="Update/approve visual baseline screenshots"
-    )
